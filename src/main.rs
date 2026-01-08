6061a90e7e96d1ff5482839d24d9d79fc9c2946a
Henry Du <henry.thdu@protonmail.com>
2026-01-08 16:58:31 -0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use nn::{Layer, MLP};
 fn main() {
     let mut area = Area::new();
     let mlp = MLP::new(&mut area, 4, vec![8, 16, 32]);
-    // let input = vec![2.0, 3.0, 4.0];
-    // let output = neuron.forward(&mut area, input);
-    println!("Output: {:?}", mlp);
+    let input = vec![area.scalar(2.0), area.scalar(3.0), area.scalar(4.0)];
+    let output = mlp.forward(&mut area, input);
+    println!("Output: {:?}", output);
 }
